e15d4388592fd4187dc86ca2a49272776365c9b2
Codewithdeepanshi <way15101998@gmail.com>
2024-10-16 10:09:55 +0530

    Add ansibile and docker
    Add ansibile and docker

--- src/main/java/banking/BankingApplication.java
+++ src/main/java/banking/BankingApplication.java
@@ -1,18 +1,14 @@
+//package com.project.staragile.banking;
 package banking;
-
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class BankingApplication {
 
     public static void main(String[] args) {
-        System.out.println("Banking Application is running...");
-
-        // Keep the application running
-        while (true) {
-            try {
-                Thread.sleep(2000); // Sleep for a while (1 second)
-            } catch (InterruptedException e) {
-                // Handle the interruption
-                Thread.currentThread().interrupt(); // Restore interrupted status
-            }
-        }
+        SpringApplication app = new SpringApplication(BankingApplication.class);
+        app.setAdditionalProfiles("dev"); // Change 'dev' to 'prod' for production
+        app.run(args);
     }
+  
 }
