d2c72cc615155bc97c498c07d1f3726311a0d6fd
Deepanshi <way15101998@gmail.com>
2024-10-20 22:30:06 +0530

    dockerfile

--- src/test/java/devopsproject/microservice/DevopsprojectApplicationTests.java
+++ src/test/java/devopsproject/microservice/DevopsprojectApplicationTests.java
@@ -10,4 +10,9 @@ class DevopsprojectApplicationTests {
 	void contextLoads() {
 	}
 
+
+public class "HelloWorld" {
+    public static void main(String[] args) {
+        System.out.println("Hello, Docker World!");
+    }
 }
